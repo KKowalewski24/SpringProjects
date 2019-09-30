d98517c218fda7c40dc9e3109326edb70c67bee7
Kamil Kowalewski <szamil128@onet.pl>
2019-09-30 16:15:46 +0200

    Rename directory

---
+++ RestfulWebService/src/main/java/pl/kkowalewski/restfulwebservice/controller/GreetingController.java
@@ -0,0 +1,9 @@
+package pl.kkowalewski.restfulwebservice.controller;
+
+public class GreetingController {
+
+    /*------------------------ FIELDS REGION ------------------------*/
+
+    /*------------------------ METHODS REGION ------------------------*/
+
+}
