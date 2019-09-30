dca0102fd043e93f0e31e6fb4d4f7005943e5ee2
Kamil Kowalewski <szamil128@onet.pl>
2019-09-30 16:43:24 +0200

    Add Controller + file with paths

--- RestfulWebService/src/main/java/pl/kkowalewski/restfulwebservice/constant/AppConstants.java
+++ RestfulWebService/src/main/java/pl/kkowalewski/restfulwebservice/constant/AppConstants.java
@@ -3,6 +3,11 @@ package pl.kkowalewski.restfulwebservice.constant;
 public class AppConstants {
 
     /*------------------------ FIELDS REGION ------------------------*/
+    public static final String ROOT = "";
+    public static final String SLASH = "/";
+    public static final String GREETING = "greeting";
+
+    public static final String PATH_GREETING = SLASH + GREETING;
 
     /*------------------------ METHODS REGION ------------------------*/
 
