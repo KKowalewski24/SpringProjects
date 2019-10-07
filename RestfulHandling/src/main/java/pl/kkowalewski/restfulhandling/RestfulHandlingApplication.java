bbce633de13e9ac26cf8ff42fd8afb419ab31697
Kamil Kowalewski <szamil128@onet.pl>
2019-10-07 11:06:05 +0200

    Add model + main class

--- RestfulHandling/src/main/java/pl/kkowalewski/restfulhandling/RestfulHandlingApplication.java
+++ RestfulHandling/src/main/java/pl/kkowalewski/restfulhandling/RestfulHandlingApplication.java
@@ -1,10 +1,33 @@
 package pl.kkowalewski.restfulhandling;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import pl.kkowalewski.restfulhandling.model.Quote;
 
 @SpringBootApplication
 public class RestfulHandlingApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(RestfulHandlingApplication.class);
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) {
+        return args -> {
+            Quote quote = restTemplate.getForObject(
+                    "https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+            log.info(quote.toString());
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RestfulHandlingApplication.class, args);
