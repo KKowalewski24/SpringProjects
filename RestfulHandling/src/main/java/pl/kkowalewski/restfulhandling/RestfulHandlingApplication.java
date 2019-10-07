bf15922d2e86f76af8a69e09ea542d284a7a6f75
Kamil Kowalewski <szamil128@onet.pl>
2019-10-07 10:45:50 +0200

    fix formatting

--- RestfulHandling/src/main/java/pl/kkowalewski/restfulhandling/RestfulHandlingApplication.java
+++ RestfulHandling/src/main/java/pl/kkowalewski/restfulhandling/RestfulHandlingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestfulHandlingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestfulHandlingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulHandlingApplication.class, args);
+    }
 
 }
