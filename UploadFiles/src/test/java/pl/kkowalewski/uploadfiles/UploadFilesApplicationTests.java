cc60dcbaa43bdc0ca280b64045727805ec4fd851
Kamil Kowalewski <szamil128@onet.pl>
2019-11-21 20:34:56 +0100

    Initial commit of UploadFiles

---
+++ UploadFiles/src/test/java/pl/kkowalewski/uploadfiles/UploadFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kkowalewski.uploadfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadFilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
