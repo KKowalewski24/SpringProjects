3747edb1269f64198f59009507b4c459f6bc5e31
Kamil Kowalewski <szamil128@onet.pl>
2019-11-28 21:44:51 +0100

    Initial commit of restful actuator

---
+++ RestfulActuator/src/test/java/pl/kkowalewski/restfulactuator/RestfulActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kkowalewski.restfulactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
