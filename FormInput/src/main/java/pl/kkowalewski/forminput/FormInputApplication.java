ab6422b4df9a91cffcbe4d225dd0f89ffe9d080c
Kamil Kowalewski <szamil128@onet.pl>
2019-11-28 20:14:05 +0100

    Initial commit of FormInput Project

---
+++ FormInput/src/main/java/pl/kkowalewski/forminput/FormInputApplication.java
@@ -0,0 +1,13 @@
+package pl.kkowalewski.forminput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormInputApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormInputApplication.class, args);
+    }
+
+}
---
+++ FormInput/src/test/java/pl/kkowalewski/forminput/FormInputApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kkowalewski.forminput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormInputApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
