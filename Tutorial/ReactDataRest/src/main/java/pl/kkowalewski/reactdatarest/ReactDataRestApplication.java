9badd6a362155f7c8c066d99a0297c22bc1f1cb1
Kamil Kowalewski <szamil128@onet.pl>
2019-12-15 20:45:28 +0100

    Initial commit of RestServiceApplication and ReactDataRestApplication

---
+++ Tutorial/ReactDataRest/src/main/java/pl/kkowalewski/reactdatarest/ReactDataRestApplication.java
@@ -0,0 +1,13 @@
+package pl.kkowalewski.reactdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactDataRestApplication.class, args);
+    }
+
+}
---
+++ Tutorial/ReactDataRest/src/test/java/pl/kkowalewski/reactdatarest/ReactDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kkowalewski.reactdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
